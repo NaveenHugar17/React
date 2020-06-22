46e55a6f79bcc2080eff18b035425940a0196c8c
Naveen Hugar <naveenhugarnh21@gmail.com>
2020-06-22 19:45:20 +0530

    Assignment1

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody,
+  CardTitle } from 'reactstrap';
+
+class DishDetail extends Component{
+    constructor(props) {
+        super(props);
+    }
+
+    
+renderDish(dish) {  
+        return(
+            <div>
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>          
+        ); 
+}
+
+renderComments(comments){
+
+    if(comments!=null)
+    {
+        return(
+            <div>
+                <h4>Comments</h4>
+                <ul className="list-unstyled">
+                            {comments.map((comm) => {
+                            return (
+                                <li key={comm.id}>
+                                    <p>{comm.comment}</p>
+                                    <p>-- {comm.author}, {comm.date}</p>
+                                </li>
+                            );
+                        })}
+                </ul>    
+            </div>    
+        );
+    }
+    else
+        return(
+            <div></div>
+        );
+}
+
+render()
+{
+    const dish=this.props.dishdetail;
+    if(dish!=null)
+        return(
+            <div className="row">
+                <div  className="col-12 col-md-5 m-1">
+                    {this.renderDish(dish)}
+                </div>
+                <div  className="col-12 col-md-5 m-1">
+                    {this.renderComments(dish.comments)}
+                </div>
+
+            </div>
+
+        );
+    else
+        return(
+            <div></div>
+        )
+}
+
+}
+
+
+export default DishDetail;
